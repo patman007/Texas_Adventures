64e8c684067184d6d8798f724eb8b66e95de8030
Hernan Hernandez <hhernan83@yahoo.com>
2020-11-08 19:49:27 -0600

    Changes to the weather api trying to link the parks api to get the city

--- Weatherapp_practice_Fetch/script.js
+++ Weatherapp_practice_Fetch/script.js
@@ -13,10 +13,53 @@ let long = document.getElementById("long")
 //Button variable by Ids
 let btn = document.getElementById('btn');
 
+
+
+//Consume API Key
+let apiKey = 'pmBnVo1MoSqhZcTEhSBPhAxdaS5GQvG0lPyDCvRY'
+let endpoint = `https://developer.nps.gov/api/v1/parks?stateCode=TX&api_key=${apiKey}`
+console.log(endpoint)
+
+
+btn.addEventListener('click', event  => {
+    event.preventDefault();
+
+    //4 parts to the fetch API
+    fetch(endpoint)
+   
+    .then(function(res) {
+        console.log(res);
+
+        if(res.ok) {
+
+            return res.json();
+        }
+        throw Error('Weather API is not working !!!')
+    })   
+    
+    .then(data =>  {    
+            console.log(data.data[0])    
+             console.log (data.data[0].addresses[1].city)
+
+             const City = data.data[0].addresses[1].city
+
+             console.log(City)
+
+             return City
+    })           
+      
+    //Catch an error to show in console.log message
+    .catch(err => console.error('Error connecting', err))
+});
+console.log(data.data[0].addresses[1].city)
+
+
+
+
 //Url and API key
 // 'http://api.openweathermap.org/data/2.5/weather?q=Austin&appid=27fe525266ca77d7607f290c665c9860'
 let api_key = '27fe525266ca77d7607f290c665c9860';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=Austin&appid=${api_key}`
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${City}&appid=${api_key}`
 
 console.log(url);
 
