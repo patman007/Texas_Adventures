47747fdd3e402171a913033090611c9a1e641f06
Hernan Hernandez <hhernan83@yahoo.com>
2020-11-08 20:51:55 -0600

    Updated Weather API With boss man

--- Weatherapp_practice_Fetch/script.js
+++ Weatherapp_practice_Fetch/script.js
@@ -9,59 +9,58 @@ let lat = document.getElementById("lat");
 //LONGTITUDE as a variable
 let long = document.getElementById("long")
 
-
 //Button variable by Ids
 let btn = document.getElementById('btn');
 
+// Parks API
+let pApiKey = 'pmBnVo1MoSqhZcTEhSBPhAxdaS5GQvG0lPyDCvRY'
+let parksUrl = `https://developer.nps.gov/api/v1/parks?stateCode=TX&api_key=${pApiKey}`
 
-
-//Consume API Key
-let apiKey = 'pmBnVo1MoSqhZcTEhSBPhAxdaS5GQvG0lPyDCvRY'
-let endpoint = `https://developer.nps.gov/api/v1/parks?stateCode=TX&api_key=${apiKey}`
-console.log(endpoint)
-
+// Weather API
+let wApi_key = '27fe525266ca77d7607f290c665c9860';
+let weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=CITY_NAME&appid=${wApiKey}`
 
 btn.addEventListener('click', event  => {
     event.preventDefault();
 
     //4 parts to the fetch API
-    fetch(endpoint)
-   
+    fetch(parksUrl)
     .then(function(res) {
         console.log(res);
-
         if(res.ok) {
-
-            return res.json();
+            return res.json().data[0].addresses[1].city;
         }
-        throw Error('Weather API is not working !!!')
+        // throw Error('Weather API is not working !!!')
     })   
-    
     .then(data =>  {    
-            console.log(data.data[0])    
-             console.log (data.data[0].addresses[1].city)
+        // do your second fetch using specific city from "data"
+        // should return some weather data
+        fetch(url)
+        .then()
+        .then()
+        .catch()
 
-             const City = data.data[0].addresses[1].city
+        console.log(data.data[0])    
+        console.log (data.data[0].addresses[1].city)
 
-             console.log(City)
-
-             return City
+        const City = data.data[0].addresses[1].city
+        console.log('City is ', City)
+        city.innerHTML = 'City: '+City
+    })
+    .then(weatherdata => {
+        // show both the park and weather data on browser
     })           
-      
     //Catch an error to show in console.log message
     .catch(err => console.error('Error connecting', err))
 });
-console.log(data.data[0].addresses[1].city)
+// console.log(data.data[0].addresses[1].city)
 
-
-
-
+let City = document.getElementById('city')
 //Url and API key
+console.log('City is ', City)
 // 'http://api.openweathermap.org/data/2.5/weather?q=Austin&appid=27fe525266ca77d7607f290c665c9860'
-let api_key = '27fe525266ca77d7607f290c665c9860';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${City}&appid=${api_key}`
 
-console.log(url);
+console.log('Url is ', url);
 
 // api.openweathermap.org/data/2.5/find?q=London&units=metric
 
@@ -70,30 +69,23 @@ console.log(url);
 //the fetch steps statement will be initiated
 btn.addEventListener('click', event  => {
     event.preventDefault();
-
     //4 parts to the fetch API
     fetch(url)
-   
     .then(function(res) {
         console.log(res);
-
         if(res.ok) {
-
             return res.json();
         }
-        throw Error('Weather API is not working !!!')
+        // throw Error('Weather API is not working !!!')
     })   
-    
     .then(data =>  {        
         console.log('data is : ', data )       
-        city.innerHTML = 'City: '+data.name
+        // city.innerHTML = 'City: '+data.name
         city_id.innerHTML = 'City ID: '+data.id                     
         temp.innerHTML = 'Temperature: '+ data.main.temp        
         lat.innerHTML = 'Latitude: '+data.coord.lat
         long.innerHTML = 'Longitude: '+data.coord.lon                      
-    })           
-                                         
-
+    })
     //Catch an error to show in console.log message
     .catch(err => console.error('Error connecting', err))
 });
