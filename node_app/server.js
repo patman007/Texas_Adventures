f523c6f36d66526449c4ee8c1633c459d1367825
Hernan Hernandez <hhernan83@yahoo.com>
2020-12-04 11:00:05 -0600

    updated application with axios

--- node_app/server.js
+++ node_app/server.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const app = express()
 const mongoose= require('mongoose')
-const bodyparser= require('body-parser')
+const bodyParser= require('body-parser')
 const fetch = require('node-fetch')
+const axios = require('axios');
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// // parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// // parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+// const Bluebird = require('bluebird');
+ 
+// fetch.Promise = Bluebird;
 app.use(express.static('public'))
 
 PORT= process.env.PORT || 3000
+
+
+  // https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=hNdp9FdWfJRXxC48PNwvA1ffZxzbokHVs2P7Dzfi
+//   let pApi_Key = 'pmBnVo1MoSqhZcTEhSBPhAxdaS5GQvG0lPyDCvRY'
+let pApi_Key = 'hNdp9FdWfJRXxC48PNwvA1ffZxzbokHVs2P7Dzfi'
+  let parksUrl = `https://developer.nps.gov/api/v1/parks?stateCode=TX&api_key=${pApi_Key}`
+  let fullData=[]
+  // Weather API
+  let wApi_Key = '27fe525266ca77d7607f290c665c9860';
+  let weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=CITY_NAME&units=imperial&appid=${wApi_Key}`
+
 
 app.get('/', (req,res)=>{
     res.render('login.ejs')
@@ -16,63 +40,66 @@ app.get('/', (req,res)=>{
 app.get('/search', (req,res)=>{
     res.render('search.ejs')
 })
+app.get('/city',(req,res)=>{
+let city= req.query.city
+
+let wApi_Key = '27fe525266ca77d7607f290c665c9860';
+let weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${wApi_Key}`
+
+fetch(weatherUrl)
+.then(response=>{
+return response.json()
+})
+.then(data=>{
+console.log(data)
+res.render('results.ejs')
+})
+.catch(err=>{
+    console.log('Error connecting to api: ', err)
+    res.render('search.ejs')
+})
+})
 
 app.get('/random',(req,res)=>{
-    let pApi_Key = 'pmBnVo1MoSqhZcTEhSBPhAxdaS5GQvG0lPyDCvRY'
-    let parksUrl = `https://developer.nps.gov/api/v1/parks?stateCode=TX&api_key=${pApi_Key}`
-    let fullData=[]
-    // Weather API
-    let wApi_Key = '27fe525266ca77d7607f290c665c9860';
-    let weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=CITY_NAME&units=imperial&appid=${wApi_Key}`
+ 
+axios.get(parksUrl)
+.then(response=> {
+  
+    return response.data
     
+    // return response.json()
+})
+.then(data=>{
 
+
+
+ 
+    var randomCityIndex = Math.floor(Math.random()*data.data.length -1) + 1
+    // console.log('This is the index:',data.data[randomCityIndex])
+
+    const City = data.data[randomCityIndex].addresses[0].city
     
-        //4 parts to the fetch API
-        fetch(parksUrl)
-        .then(function(res) {
-           
-            if(res.ok) {
-                // return res.json().data[0].addresses[1].city;
-                console.log(res.json())
-                return res
-            }
-            // throw Error('Weather API is not working !!!')
-        })   
-        .then(data =>  {    
-            // do your second fetch using specific city from "data"
-            // should return some weather data
-          
-            console.log(data)
-            var randomCityIndex = Math.floor(Math.random()*data.data.length -1) + 1
-           
-            // National Park City
-            const City = data.data[randomCityIndex].addresses[0].city
-            fullData.push(data.data[randomCityIndex])
-           
-          
+ console.log(data.data[randomCityIndex])
     
-    
-            let apiKey = '27fe525266ca77d7607f290c665c9860'
-            let url= `http://api.openweathermap.org/data/2.5/weather?q=${City}&units=imperial&appid=${apiKey}`
-            fetch(url)
-                .then(function(res) {
-                   
-                    if(res.ok) {
-                        return res.json();
-                    }
-                    // throw Error('Weather API is not working !!!')
-                })   
-                .then(Weather =>  {        
-                      fullData.push(Weather)                                
-                })
-                
-        })
-         .then(data=>{
-            console.log(fullData)
-         })       
-        //Catch an error to show in console.log message
-        .catch(err => console.error('Error connecting', err))
-        
+    let wApi_Key = '27fe525266ca77d7607f290c665c9860';
+    let weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${City}&units=imperial&appid=${wApi_Key}`
+
+    axios.get(weatherUrl)
+    .then(response=>{
+       return response
+    })
+    .then(data=>{
+        console.log(data.data)
+    })
+    .catch(err=>[
+        console.log('Error recieving data from weather api: ', err)
+    ])
+    res.render('results.ejs')
+})
+.catch(err=>{
+    console.log('Error connecting to api: ', err)
+
+})
     });
 
 
