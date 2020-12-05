4a6676dfec1d468a28a6f97d6a9ec8c0cd03f88e
Hernan Hernandez <hhernan83@yahoo.com>
2020-12-04 18:34:20 -0600

    working city and random calls as well as results display.

--- node_app/server.js
+++ node_app/server.js
@@ -4,20 +4,15 @@ const mongoose= require('mongoose')
 const bodyParser= require('body-parser')
 const fetch = require('node-fetch')
 const axios = require('axios');
-
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
-
 // // parse some custom thing into a Buffer
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
-
 // // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
 // const Bluebird = require('bluebird');
- 
 // fetch.Promise = Bluebird;
 app.use(express.static('public'))
-
 PORT= process.env.PORT || 3000
 
 
@@ -29,46 +24,52 @@ let pApi_Key = 'hNdp9FdWfJRXxC48PNwvA1ffZxzbokHVs2P7Dzfi'
   // Weather API
   let wApi_Key = '27fe525266ca77d7607f290c665c9860';
   let weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=CITY_NAME&units=imperial&appid=${wApi_Key}`
-
-
 app.get('/', (req,res)=>{
     res.render('login.ejs')
 })
-
-
-
 app.get('/search', (req,res)=>{
     res.render('search.ejs')
 })
-
-
 app.get('/favorites',(req,res)=>{
     res.render('favorites.ejs')
 })
-
-
-
+//
 app.get('/city',(req,res)=>{
 let city= req.query.city
-
+console.log(city)
 let wApi_Key = '27fe525266ca77d7607f290c665c9860';
 let weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${wApi_Key}`
 let pApi_Key = 'hNdp9FdWfJRXxC48PNwvA1ffZxzbokHVs2P7Dzfi'
-  let parksUrl = `https://developer.nps.gov/api/v1/parks?stateCode=TX&q=${city}api_key=${pApi_Key}`
+  let parksUrl = `https://developer.nps.gov/api/v1/parks?stateCode=TX&q=${city}&api_key=${pApi_Key}`
+// console.log(parksUrl)
 
+// calling parks url
   axios.get(parksUrl)
+// digging into the response to get our data object
   .then(response=>{
-      return response
+        // console.log(response.data.data[0])
+        // if the results turn up as a non existent city or 0 we will be redirected to the error page
+      if(response.data.total <=0 ){
+          throw Error('No data recieved not a valid city or no parks fouond ')
+      }else{
+          //returning response
+      return response.data.data[0]
+      }
   })
   .then(data=>{
-      console.log(data)
+    let park= data
+    //   console.log(data)
+                // calling out weather api
                 axios.get(weatherUrl)
             .then(response=>{
-            return response.json()
+            return response.data
             })
-            .then(data=>{
+            .then(weatherdata=>{
+            console.log(weatherdata)
+            var results={weatherdata,park}
+            //sending our results into our results ejs
             console.log(data)
-            res.render('results.ejs')
+            res.render('results.ejs',{results})
             })
             .catch(err=>{
                 console.log('Error connecting to api: ', err)
@@ -77,6 +78,7 @@ let pApi_Key = 'hNdp9FdWfJRXxC48PNwvA1ffZxzbokHVs2P7Dzfi'
   })
   .catch(err=>{
       console.log('No city found: ', err)
+    
       res.render('error.ejs')
   })
 
@@ -87,42 +89,47 @@ app.get('/random',(req,res)=>{
  
     axios.get(parksUrl)
     .then(response=> {
-    
-        return response.data
-        
-        // return response.json()
+        if(response.data.total <=0 ){
+            throw Error('No data recieved not a valid city or no parks found ')
+        }else{
+            //returning response
+        return response.data.data
+        }
     })
     .then(data=>{
-
-
-
-        
-            var randomCityIndex = Math.floor(Math.random()*data.data.length -1) + 1
-            // console.log('This is the index:',data.data[randomCityIndex])
-
-            const City = data.data[randomCityIndex].addresses[0].city
+       
+         var randomCityIndex = Math.floor(Math.random()*data.length -1 ) + 1
+         // console.log('This is the index:',data.data[randomCityIndex])
+            // console.log(data.length)
+            // console.log(data[randomCityIndex])
+          const City = data[randomCityIndex].addresses[0].city
+           let park= data[randomCityIndex]
+         console.log(park)
+        // console.log(data.data[randomCityIndex])
             
-            console.log(data.data[randomCityIndex])
-            
-            let wApi_Key = '27fe525266ca77d7607f290c665c9860';
-            let weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${City}&units=imperial&appid=${wApi_Key}`
+         let wApi_Key = '27fe525266ca77d7607f290c665c9860';
+         let weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${City}&units=imperial&appid=${wApi_Key}`
 
-        axios.get(weatherUrl)
-        .then(response=>{
-            return response
-        })
-        .then(data=>{
-            console.log(data.data)
-        })
-        .catch(err=>[
-            console.log('Error recieving data from weather api: ', err)
-        ])
-        res.render('results.ejs')
-    })
+                axios.get(weatherUrl)
+                .then(response=>{
+                    return response.data
+                })
+                .then(weatherdata=>{
+                    console.log(weatherdata)
+                    var results = {weatherdata, park}
+                    res.render('results.ejs',{results})
+                })
+                .catch(err=>{
+                    console.log('Error recieving data from weather api: ', err)
+                    res.render('error.ejs')
+                })
+                
+     })
     .catch(err=>{
-        console.log('Error connecting to api: ', err)
+         console.log('Error connecting to api: ', err)
+        res.render('error.ejs')
 
-    })
+     })
     });
 
 
